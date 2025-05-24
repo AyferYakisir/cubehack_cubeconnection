b70e6a177bba3966385b13146f8229295ced79c8
ayksr <yakisirayfer@gmail.com>
2025-05-25 01:19:01 +0300

    feat(entry): Uygulama giriş noktası HomePage olarak ayarlandı
    Uygulama başlatıldığında ana sayfa olarak HomePage widget'ı yüklenmektedir.
    Bu ayar, main.dart dosyasında MaterialApp'in home parametresi üzerinden yapılmıştır.
    
    Dosya:
    - lib/main.dart
    
    Refs: #homepage #entry #routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:cubeincubation_mobile_hackathon_2025/views/splash_secreen.dart';
+import 'package:cubeincubation_mobile_hackathon_2025/views/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'controllers/splash_controller.dart';
-import 'blocs/splash/splash_bloc.dart';
+import 'controllers/home_controller.dart';
+import 'blocs/home/home_bloc.dart';
+import 'blocs/home/home_event.dart';
 
 void main() {
   runApp(const CubeConnectApp());
@@ -16,8 +17,8 @@ class CubeConnectApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: BlocProvider(
-        create: (_) => SplashBloc(SplashController()),
-        child: const SplashScreen(),
+        create: (_) => HomeBloc(HomeController())..add(LoadInitiatives()),
+        child: const HomePage(),
       ),
     );
   }
